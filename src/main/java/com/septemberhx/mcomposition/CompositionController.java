743b1f18eb234a7ad9c43d99409fb606d7f439a6
SampleService <tianjianshan@outlook.com>
2019-12-10 08:48:40 +0000

    cainiao-service-jindong-service-729924250

---
+++ src/main/java/com/septemberhx/mcomposition/CompositionController.java
@@ -0,0 +1,31 @@
+package com.septemberhx.mcomposition;
+
+import com.septemberhx.mclient.annotation.MApiFunction;
+import com.septemberhx.mclient.annotation.MRestApiType;
+import com.septemberhx.mclient.annotation.MFunctionType;
+import com.septemberhx.mclient.base.MObject;
+import com.septemberhx.common.base.MResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompositionController extends MObject {
+
+    @MFunctionType
+com.sampleservice.cainiaoservice.controller.MainController controller0;
+@MFunctionType
+com.sampleservice.jindongservice.controller.MainController controller1;
+
+
+    @RequestMapping(path = "/test", method = RequestMethod.POST)
+    @MApiFunction
+    @MRestApiType
+    public MResponse test(@RequestBody MResponse body) {
+        MResponse r0 = controller0.payFunction(body);
+MResponse r1 = controller1.marketFunction(r0);
+return r1;
+
+    }
+}
